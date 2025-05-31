b663da1305f42403a8131e733e32d491ff3b3c16
Deepali Shrivastav <deepalishrivastav2302@gmail.com>
2025-05-31 18:42:57 +0530

    updated urls.py

--- exp_tracker/urls.py
+++ exp_tracker/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('expenses', views.ExpenseListView.as_view(), name='expenses'),
     path('accounts/register/', views.register, name='register'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
