9cf2678c7770992735b3f3ec1c6035c52eea30ea
Deepali Shrivastav <deepalishrivastav2302@gmail.com>
2025-05-31 18:45:20 +0530

    updated forms.py

--- exp_tracker/forms.py
+++ exp_tracker/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Expense
 
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Remove the username similarity validator
+        self.fields['password1'].validators = [v for v in self.fields['password1'].validators 
+                   
 
 class ExpenseForm(forms.ModelForm):
     long_term = forms.BooleanField(required=False)
